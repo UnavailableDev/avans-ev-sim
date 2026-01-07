fe5077097c163c167826954dfe5bfc8c39645469
UnavailableDev <69792062+UnavailableDev@users.noreply.github.com>
2026-01-07 16:07:59 +0100

    More consise charging_station code

--- src/world/charging_station.cpp
+++ src/world/charging_station.cpp
@@ -3,7 +3,6 @@
 namespace world {
 
 void ChargingStation::HandleArrival(std::shared_ptr<vehicles::Vehicle> v) {
-  // If the arriving vehicle is an EV, add it to the charging queue
   auto ev_ptr = std::dynamic_pointer_cast<vehicles::EV>(v);
   if (ev_ptr) {
     AddToQueue(ev_ptr);
@@ -11,8 +10,6 @@ void ChargingStation::HandleArrival(std::shared_ptr<vehicles::Vehicle> v) {
 }
 
 void ChargingStation::Update() {
-  // Here you could implement logic to charge vehicles in the queue
-  // For simplicity, we'll just pop one vehicle from the queue per update
   auto ev = PopFromQueue();
   if (ev) {
     // Simulate charging by setting its state of charge to 1.0
@@ -31,15 +28,12 @@ void ChargingStation::AddToQueue(std::shared_ptr<vehicles::EV> ev) {
 
 std::shared_ptr<vehicles::EV> ChargingStation::PopFromQueue() {
   std::lock_guard<std::mutex> lock(queueMutex_);
-  if (evQueue_.empty()) {
-    return nullptr;
-  }
+  if (evQueue_.empty()) return nullptr;
   
   auto ev = evQueue_.front();
   evQueue_.pop();
   chargeCount_++;
-  double soc = ev->GetStateOfCharge();
-  totalCharged_ += soc * ev->GetModel().GetBatteryCapacity_kWh();
+  totalCharged_ += ev->GetStateOfCharge() * ev->GetModel().GetBatteryCapacity_kWh();
   return ev;
 }
 
